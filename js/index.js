d0be035fc7a20cae6026e790127cff2316fbe61d
irinka1369 <irinka1369@gmail.com>
2019-08-11 15:24:19 +0000

    add counter of characters

--- js/index.js
+++ js/index.js
@@ -2,4 +2,16 @@
 
 setInterval( ()=> $('.clock').html((new Date()).toLocaleTimeString()), 1000 );
 
+$(document).ready( () => {
+
+  $('.feedback-input').on('keyup', event => {
+    let post = $(event.currentTarget).val();
+    let remaining = 500 - post.length;
+    $('.characters').html(remaining);
+  });
+
+  $('.feedback-input').focus();
+
+ });
+
 
