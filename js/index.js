7bf768a33754e7342bf8cf320199c84542aa135c
irinka1369 <irinka1369@gmail.com>
2019-08-12 13:22:59 +0000

    test

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ setInterval( ()=> $('.clock').html((new Date()).toLocaleTimeString()), 1000 );
 $(document).ready( () => {
 
   $('#textarea').on('keyup', event => {
-    let post = $(event.currentTarget).val();
-    let remaining = 500 - post.length;
+    const post = $(event.currentTarget).val();
+    const remaining = 500 - post.length;
     $('.characters').html(remaining);
   });
 
