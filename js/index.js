dda74dcbb2b141b7032b816688be4721a60ac97e
irinka1369 <irinka1369@gmail.com>
2019-08-12 08:18:56 +0000

    instal server

--- js/index.js
+++ js/index.js
@@ -15,3 +15,23 @@ $(document).ready( () => {
  });
 
 
+document.querySelector('.contact-form input[type=submit]')
+    .addEventListener('click', accept);
+
+function accept(e) {
+    e.preventDefault();
+    fetch(' accept', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            tvoje_Ime: document.querySelector('.login-form input[name=tvoje_ime]').value,
+            email: document.querySelector('.login-form input[name=email]').value,
+            tvoja_Poruka: document.querySelector('.login-form input[name=tvoja_poruka]').value
+        })
+    })
+        .then(_ => document.querySelector('.login-form').reset());
+}
+
