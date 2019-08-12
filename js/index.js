0734878ef32cfb6b7c63e91d3dc9960b89548b84
irinka1369 <irinka1369@gmail.com>
2019-08-12 08:50:30 +0000

    working formspring

--- js/index.js
+++ js/index.js
@@ -15,20 +15,3 @@ $(document).ready( () => {
  });
 
 
-//document.querySelector('.login-form button[type=submit]').addEventListener('click', login);
-
-//function login(e) {
-//  e.preventDefault();
-//    fetch('login', {
-//        method: 'POST',
-//        headers: {
- //           'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
- //       },
- //       body: JSON.stringify({
- //           ime: document.querySelector('.login-form input[name=tvoje_ime]').value,
- //           email: document.querySelector('.login-form input[name=email]').value,
- //           poruka: document.querySelector('.login-form input[name=tvoja_poruka]').value
- //       })
- //   })
- //       .then(_ => document.querySelector('.login-form').reset());}
